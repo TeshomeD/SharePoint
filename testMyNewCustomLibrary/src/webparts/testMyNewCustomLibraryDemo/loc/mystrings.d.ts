48d5b2977adde7d30bfa50dad5198f50397b483e
Teshome <linkteshome2world@gmail.com>
2023-03-30 01:22:19 -0400

    implement how to use our custom library

---
+++ testMyNewCustomLibrary/src/webparts/testMyNewCustomLibraryDemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ITestMyNewCustomLibraryDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'TestMyNewCustomLibraryDemoWebPartStrings' {
+  const strings: ITestMyNewCustomLibraryDemoWebPartStrings;
+  export = strings;
+}
