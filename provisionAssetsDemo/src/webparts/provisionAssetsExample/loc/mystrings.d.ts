a4081bfdf5ae368bdfe6b8a0ce09b7e9297076cd
Teshome <linkteshome2world@gmail.com>
2023-03-30 00:59:17 -0400

    provision asset demo

---
+++ provisionAssetsDemo/src/webparts/provisionAssetsExample/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IProvisionAssetsExampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ProvisionAssetsExampleWebPartStrings' {
+  const strings: IProvisionAssetsExampleWebPartStrings;
+  export = strings;
+}
